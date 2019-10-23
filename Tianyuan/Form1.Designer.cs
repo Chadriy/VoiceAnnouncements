bb1c6e7155fbb4aeef7512eeb4e24cf0d864539f
caijingxin <962547845@qq.com>
2019-10-23 18:05:12 +0800

    commit

---
+++ Tianyuan/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Tianyuan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::Tianyuan.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(0, -25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 237);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(0, 200);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(519, 279);
+            this.listBox1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(518, 474);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "天原危化品车辆管理平台语音播报系统";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Tianyuan/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using WebSocket4Net;
+using System.Threading;
+using System.Speech.Synthesis;
+using SuperSocket.ClientEngine;
+
+namespace Tianyuan
+{
+    public partial class Form1 : Form
+    {
+        WebSocket websocket;
+        int flag = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void websocket_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            if (!e.Message.Equals(" "))
+                this.listBox1.Invoke(new EventHandler(ShowMessage), e.Message);
+
+        }
+
+        private void ShowMessage(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Add(DateTime.Now.ToLocalTime().ToString() +"      " + sender.ToString());
+            if (listBox1.Items.Count > 10)
+            {
+                listBox1.Items.RemoveAt(0);
+            }
+            SpeechSynthesizer speaker = new SpeechSynthesizer();
+            speaker.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult, 2, System.Globalization.CultureInfo.CurrentCulture);
+            speaker.Rate = 1;
+            speaker.Volume = 100;
+            speaker.SpeakAsync(sender.ToString());
+        }
+
+
+        void websocket_Error(object sender, ErrorEventArgs e)
+        {
+            if (flag == 0)
+            {
+                this.listBox1.Invoke(new EventHandler(ShowMessage), "网络异常");
+                flag = 1;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            websocket = new WebSocket("wss://whpgl.ybty.com/websocket");
+            websocket.MessageReceived += websocket_MessageReceived;
+            websocket.Error += websocket_Error;
+            while(websocket.State== WebSocketState.None)
+                 websocket.Open();
+            this.listBox1.Invoke(new EventHandler(ShowMessage), "服务器连接中");
+            while (websocket.State== WebSocketState.Open)
+                timer1.Enabled = true;
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (websocket.State != WebSocketState.Open&& websocket.State != WebSocketState.Connecting)
+            {
+                websocket = new WebSocket("wss://whpgl.ybty.com/websocket");
+                websocket.MessageReceived += websocket_MessageReceived;
+                websocket.Error += websocket_Error;
+                websocket.Open();
+            }
+            else
+                flag = 0;
+        }
+    }
+}
